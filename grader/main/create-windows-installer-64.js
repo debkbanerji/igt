b79dc9ee0407fd5ec6eda4c68f812b3ae1b87f0f
deb <debkbanerji@gmail.com>
2018-08-05 15:55:08 -0700

    Update windows installer creator script

--- grader/main/create-windows-installer-64.js
+++ grader/main/create-windows-installer-64.js
@@ -1,5 +1,6 @@
 const electronInstaller = require('electron-winstaller');
 const {execSync} = require('child_process');
+const pjson = require('./package.json');
 
 let command = 'rcedit.exe \"../release-builds/igt-win32-x64/igt.exe\" --set-icon \"src/favicon.ico\" --set-version-string \"FileDescription\" \"IGT\" --set-version-string \"ProductName\" \"IGT\" --set-version-string \"CompanyName\" \"Deb Banerji\" --set-version-string \"LegalCopyright\" \"Copyright Deb Banerji 2017\"';
 execSync(command);
@@ -8,7 +9,8 @@ resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: '../release-builds/igt-win32-x64',
     outputDirectory: '../installers/windows-64',
     authors: 'Deb Banerji',
-    exe: 'igt.exe'
+    exe: 'igt.exe',
+    version: pjson.version
 });
 
 resultPromise.then(() => console.log("Successfully Created Installer!"), (e) => console.log(`No dice: ${e.message}`));
